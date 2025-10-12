b10180b214ffc6e490eda32ba881fb99a8d89df8
junkyedh <22520393@gm.uit.edu.vn>
2025-10-12 23:14:03 +0700

    git init

---
+++ src/app/routes/MainRoutes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+import PageNotFound from "../layouts/PageNotFound";
+
+export default function MainRoutes() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
